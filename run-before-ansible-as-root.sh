495b2fa808ffd325f9544a6978d6a33712926ffc
workstation <workstation@debianhost>
2021-04-25 14:45:00 +0530

    Starting with shell intials

---
+++ run-before-ansible-as-root.sh
@@ -0,0 +1,9 @@
+# run from root
+echo replaceWithPassBeforeRunning | sudo -S sh -c 
+"sed -i '/sudo   ALL=(ALL:ALL)/c\%sudo   ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers 
+&& usermod -aG sudo $DEFAULT_USER"
+
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+sudo apt install ansible
