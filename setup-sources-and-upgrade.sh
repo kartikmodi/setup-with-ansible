28f5ec87cfb978a0c176d194773893ba77e90acd
kartikmodi <contact@kartikmodi.com>
2021-04-25 23:11:32 +0530

    Base TO start with

--- setup-sources-and-upgrade.sh
+++ setup-sources-and-upgrade.sh
@@ -1,11 +1,3 @@
-# run from root
-$DEFAULT_USER=workstation
-su  <<!
-replaceYourPasswordHere
-sed -i '/sudo\tALL=(ALL:ALL)/c\%sudo\tALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
-/sbin/usermod -aG sudo $DEFAULT_USER
-!
-
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.orig
 sudo cp ./roles/base/files/distribution_packages/debian_sources.list /etc/apt/sources.list
 sudo apt update
---
+++ sudoers-entry.sh
@@ -0,0 +1,9 @@
+DEFAULT_USER=workstation
+su <<!
+replaceYourPasswordHere
+sed -i '/sudo\tALL=(ALL:ALL)/c\%sudo\tALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
+/sbin/usermod -aG sudo $DEFAULT_USER
+!
+
+# need a reboot after this
+# systemctl reboot
