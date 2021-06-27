b1c59274191298b9afd5cdc685b15a8765230d81
kartikmodi <contact@kartikmodi.com>
2021-06-27 17:48:43 +0530

    intitial shell scripts

--- fresh_os_setup_scripts/2.mba_specific.sh
+++ fresh_os_setup_scripts/2.mba_specific.sh
@@ -1,12 +1,12 @@
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.orig
 sudo cp ../roles/root_user/files/distribution_packages/debian_sources_deb_org-buster.list /etc/apt/sources.list
 sudo apt update
+sudo apt install -t buster-backports curl wget apt-transport-https dirmngr
 # Debian Document Link - https://wiki.debian.org/InstallingDebianOn/Apple/MacBookAir/6-2
 # https://wiki.debian.org/wl for BCM4360 in air
 # Enable backports to install
 
 # Using uname -r will give exact kernel package version. Stick to this else backports will install latest
-sudo apt install \
+sudo apt install -t buster-backports \
   dkms build-essential linux-headers-$(uname -r) linux-image-$(uname -r) \
   firmware-linux-nonfree firmware-linux-free firmware-linux \
   broadcom-sta-dkms broadcom-sta-source broadcom-sta-common wireless-tools
