c49380351d695709150f96a48c9d5598537737c1
kartikmodi <kartik.modi2009@gmail.com>
2021-09-29 00:38:35 -0400

    Updating more packages

--- fresh_os_setup_scripts/2.virtual-box-guest-specific.sh
+++ fresh_os_setup_scripts/2.virtual-box-guest-specific.sh
@@ -1,2 +1,8 @@
 sudo apt -y install curl wget apt-transport-https dirmngr
 sudo apt -y install dkms build-essential linux-headers-$(uname -r)
+# Mount the iso of vbox guest additions now
+
+# sudo mount /dev/cdrom /mnt/guestadditions if required
+sudo sh ./VBoxLinuxAdditions.run
+
+# lsmod | grep vboxguest # to check and confirm
