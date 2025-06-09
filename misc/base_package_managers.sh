36e48dc9ce427902b691d2ce37c3dd901d196b6c
Kartik Modi <kartik+git@modi.dev>
2025-06-09 00:20:40 -0400

    Update base_package_managers.sh

--- misc/base_package_managers.sh
+++ misc/base_package_managers.sh
@@ -8,7 +8,7 @@ echo 'experimental-features = nix-command flakes' | sudo tee -a /etc/nix/nix.con
 nix profile install github:nix-community/home-manager
 
 # If error: getting status of /nix/var/nix/daemon-socket/socket: Permission denied in above command 
-sudo usermod -aG nix-users workstation 
+sudo usermod -aG nix-users workstation # This might be avoided with authorized users in /etc/nix/nix.conf
 #then relogin and try again
 
 #Install flatpak to install IDE, and basic stuff
